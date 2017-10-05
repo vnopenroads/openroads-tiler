1e99b9152958678cbe6549c9f5be951fda72110d
Matthew Hanson <matt.a.hanson@gmail.com>
2017-10-05 00:52:57 -0400

    update envvars

--- cron.sh
+++ cron.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+echo export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID >> .env
+echo export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY >> .env
 echo export DATABASE_URL=$DATABASE_URL >> .env
-echo export S3_BUCKET=$S3_BUCKET >> .env
+echo export S3_TEMPLATE=$S3_TEMPLATE >> .env
 echo export PATH=$PATH:/usr/local/bin >> .env
 echo '*/5 * * * * . /opt/app/.env; cd /opt/app; node index.js >> /opt/app/cron.log 2>&1' | crontab
 cron start -f
